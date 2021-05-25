5516b9d4012da6c40a042942a5857a94b8402a90
Johannes  Foss Abrahamsen <johannesfossabrahamsen@Johanness-MacBook-Pro.local>
2021-05-24 22:50:47 -0500

    Checking in my hw04 folder

--- hw04/your_task/js/scripts.js
+++ hw04/your_task/js/scripts.js
@@ -16,69 +16,111 @@ const search = (ev) => {
     }
 }
 
-
-const getTracks = (term) => {
-    console.log(`
-        get tracks from spotify based on the search term
-        "${term}" and load them into the #tracks section 
-        of the DOM...`);
-    
-    /*
-    let url = baseURL + `?type=track&q=BTS${term}&limit1`;
-    let i = 0;
-    while (i < 5){
-        fetch(url)
-            .then((response) => {
-                return response.json()
-            })
-            .then(tracks => {
-
-
-                document.querySelector("#tracks").innerHTML =
-                `<section class="track-item preview" data-preview-track="${tracks[i].preview_url}">
-                    <img src="${tracks[i].image_url}">
-                    <i class="fas play-track fa-play" aria-hidden="true"></i>
-                    <div class="label">
-                        <h3>Black Swan</h3>
-                        <p>
-                            "${tracks[i].name}"
-                        </p>
-                    </div>
-                </section>`
-        i += 1;
-        })   
+const playTrack = (ev) => {
+    const elem = ev.currentTarget;
+    const previewURL = elem.dataset.previewTrack;
+    if (previewURL){
+        audioPlayer.setAudioFile(previewURL);
+        audioPlayer.play();
     }
-    */
+    else {
+        console.log('no preview available for this track');
+    }
+    document.querySelector('footer .track-item').innerHTML = elem.innerHTML;
 };
 
 
+const getTracks = (term) => {
+    let i = 0
+    let url = `https://www.apitutor.org/spotify/simple/v1/search?type=track&q=${term}&limit=5`
+    fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then(tracks => {
+            if (tracks.length == 0) {
+                document.querySelector("#tracks").innerHTML =
+                `<section id="tracks">No tracks found that matches your search criteria.</section>`
+            }
+            else {
+                document.querySelector("#tracks").innerHTML = ''
+                for (const track of tracks) {
+                    if (i < 5){
+                        document.querySelector("#tracks").innerHTML +=
+                        `<section class="track-item preview" data-preview-track=${track.preview_url} onclick="playTrack(event);">
+                            <img src=${track.album.image_url}>
+                            <i class="fas play-track fa-play" aria-hidden="true"></i>
+                            <div class="label">
+                                <h3>${track.name}</h3>
+                                <p>
+                                    ${track.artist.name}
+                                </p>
+                            </div>
+                        </section>`
+                        i += 1 
+                    }
+                }   
+            }   
+        })
+};
+
 const getAlbums = (term) => {
-    console.log(`
-        get albums from spotify based on the search term
-        "${term}" and load them into the #albums section 
-        of the DOM...`);
+    let url = `https://www.apitutor.org/spotify/simple/v1/search?type=album&q=${term}`
+    fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then(albums => {
+            if (albums.length == 0) {
+                document.querySelector("#albums").innerHTML =
+                `<section id="albums">No albums were returned.</section>`
+            }
+            else {
+                document.querySelector("#albums").innerHTML = ''
+                for (const album of albums){
+                    document.querySelector("#albums").innerHTML +=
+                    `<section class="album-card" id=${album.id}>
+                        <div>
+                            <img src=${album.image_url}>
+                            <h3>${album.name}</h3>
+                            <div class="footer">
+                                <a href= target=${album.spotify_url}"_blank">
+                                    view on spotify
+                                </a>
+                            </div>
+                        </div>
+                    </section>`
+                }
+            }
+        })
 };
 
 
 const getArtist = (term) => {    
-    let url = baseURL + `?type=artist&q=${term}&limit1`;
+    let url = baseURL + `?type=artist&q=${term}&limit=1`;
     fetch(url)
         .then((response) => {
             return response.json()
         })
         .then(artist => {
-            document.querySelector("#artist").innerHTML =
-            `<section class="artist-card" id="${artist[0].id}">
-            <div>
-                <img src="${artist[0].image_url}">
-                <h3>${artist[0].name}</h3>
-                <div class="footer">
-                <a href="${artist[0].spotify_url}" target="_blank">
-                    view on spotify
-                </a>
-                </div>
-             </div>
-            </section>`
+            if (artist.length != 1) {
+                document.querySelector("#artist").innerHTML =
+                `<section id="artist">No artist found that matches your search criteria.</section>`
+            }
+            else { 
+                document.querySelector("#artist").innerHTML =
+                `<section class="artist-card" id="${artist[0].id}">
+                    <div>
+                        <img src="${artist[0].image_url}">
+                        <h3>${artist[0].name}</h3>
+                        <div class="footer">
+                            <a href="${artist[0].spotify_url}" target="_blank">
+                                view on spotify
+                            </a>
+                        </div>
+                    </div>
+                </section>`
+            }
         })   
 };
 
@@ -90,3 +132,6 @@ document.querySelector('#search').onkeyup = (ev) => {
         search();
     }
 };
+
+
+
