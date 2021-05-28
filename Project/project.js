0ded5672b2863c99b8a8b464ec16f9cd54ccfdc4
Johannes  Foss Abrahamsen <johannesfossabrahamsen@Johanness-MacBook-Pro.local>
2021-05-28 14:50:17 -0500

    project

--- Project/project.js
+++ Project/project.js
@@ -1,8 +1,13 @@
 
 /*
-<iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" 
-marginwidth="0" id="gmap_canvas" 
-src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe> <a href='https://embedmaps.org/'>google maps iframe embed</a> <script type='text/javascript' 
-src='https://embedmaps.com/google-maps-authorization/script.js?id=1581a6c7c4094ad879fd1481ce1252f307298609'></script>
-*/
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
 
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+let counter = 0;
+const size = carouselImages[2].clientWidth;
+
+carouselSlide.style.transform= 'translateX('+(-size * counter) + 'px)';
+*/
